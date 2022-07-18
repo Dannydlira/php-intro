6dbc601cb56534677bcafd04ad21e51518e22535
Dannydlira <dannydlira@gmail.com>
2022-07-18 12:22:28 -0300

    Adiconando validação de campos do formulário

--- php-intro/script.php
+++ php-intro/script.php
@@ -10,9 +10,30 @@ $categorias[] = 'idoso';
 $nome = $_POST['nome']; //aqui pode mudar para Get, assim vai mostrar no corpo da requisição e não na URL
 $idade = $_POST['idade'];
 
-var_dump($nome); //var_dump mostra o tipo da variável se é string, int.....
-var_dump($idade);
-return 0;
+if (empty($nome)) {
+    echo 'O nome não pode ser vazio';
+    return;
+}
+
+if (strlen($nome) < 3) {
+    echo 'O nome deve conter mais de 3 caractere';
+    return;
+}
+
+if (strlen($nome) > 40) {
+    echo 'O nome é muito extenso';
+    return;
+}
+
+if(!is_numeric($idade))
+{
+    echo 'Informe um número para a idade';
+    return;
+}
+
+//var_dump($nome); var_dump mostra o tipo da variável se é string, int.....
+//var_dump($idade);
+//return 0;
 
 if ($idade >= 6 && $idade <= 12) {
     for ($i = 0; $i <= count($categorias); $i++) {
